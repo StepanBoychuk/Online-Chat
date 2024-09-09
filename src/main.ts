af2335b16d22d18852ff97cfcf76d9e5230e3f73
Stepan Boychuk <stepanboichukk@gmail.com>
2024-09-09 17:25:29 +0300

    Created nestJS project and set up docker-compose environment

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { WinstonModule } from 'nest-winston';
+import { format, transports } from 'winston';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        // Erorr logger to file
+        new transports.File({
+          filename: 'tmp/errors.log',
+          level: 'error',
+          format: format.combine(format.timestamp(), format.json()),
+        }),
+        // Logger to console
+        new transports.Console({
+          format: format.combine(
+            format.cli(),
+            format.splat(),
+            format.timestamp(),
+            format.printf((info) => {
+              return `${info.timestamp} ${info.level}: ${info.message}`;
+            }),
+          ),
+        }),
+      ],
+    }),
+  });
+  await app.listen(process.env.API_PORT);
+}
+bootstrap();
